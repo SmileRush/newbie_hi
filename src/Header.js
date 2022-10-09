a8f502d2940732b9c9ebdbc0dc87a417ee5b1503
SmileRush <91185902+SmileRush@users.noreply.github.com>
2022-10-09 21:54:20 +0900

    import, export // 변수 // 함수

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      헤더
+    </div>
+  );
+};
+
+export default Header
+
+
+// const 글자 = '1234'
+// console.log("string", 글자)
+
+// const 숫자 = 1234
+// console.log("number", 숫자)
+
+// const 함수 = () => {}
+// console.log("function", 함수)
+
+// const 객체 = {}
+// console.log("object", 객체)
+
+// const 배열 = []
+// console.log("array", 배열)
+
+// const 참 = true
+// console.log("boolean", 참)
+
+// const 거짓 = false
+// console.log("boolean", 거짓)
